1d4fcd616dfd0cfa9d16d0d5707d8e690250379b
Igor Babko <i.i.babko@gmail.com>
2022-09-28 12:12:16 +0300

    04. Word motions

---
+++ part1/episode04.js
@@ -0,0 +1,13 @@
+document.querySelector('nav a:nth-child(4)').click();
+
+const string = `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Nullam id dolor id nibh ultricies vehicula ut id elit.
+  Maecenas sed diam eget risus varius blandit sit amet non magna.
+  Nullam quis risus eget urna mollis ornare vel eu leo.
+  Donec ullamcorper nulla non metus auctor fringilla.
+  Donec id elit non mi porta gravida at eget metus.
+  Maecenas faucibus mollis interdum.
+`;
