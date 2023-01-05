7d06130fb11c5e7415fe4bf05f0651390cc6d3ef
Igor Babko <i.i.babko@gmail.com>
2023-01-05 12:18:33 +0200

    31. Marks

---
+++ part3/episode31.js
@@ -0,0 +1,69 @@
+import { useState, useRef } from 'react';
+import useEvent from './useEvent';
+import { MIN_DESKTOP_WIDTH, debounce } from '../utils';
+
+function isCurrentWindowWidthSmall() {
+  return window.innerWidth < MIN_DESKTOP_WIDTH;
+}
+
+function isCurrentWindowWidthBig() {
+  return window.innerWidth >= MIN_DESKTOP_WIDTH;
+}
+
+function usePopoverPosition(ref, screenChangeCallback) {
+  const [isSmallScreen, setIsSmallScreen] = useState(isCurrentWindowWidthSmall);
+  const [target, setTarget] = useState();
+  const changeWidthTimer = useRef();
+
+  function handleResize() {
+    if (!screenHasBecomeSmall() && !screenHasBecomeBig()) return;
+
+    screenChangeCallback();
+
+    clearTimeout(changeWidthTimer.current);
+
+    changeWidthTimer.current = setTimeout(
+      () => setIsSmallScreen(isCurrentWindowWidthSmall),
+      300
+    );
+  }
+
+  const debounceResize = debounce.bind(null, handleResize, 300);
+
+  useEvent('resize', debounceResize, true, window);
+
+  function move(target, offset) {
+    offset = offset || calculateTargetOffset(target);
+
+    ref.current.style.top = `${offset.top}px`;
+    ref.current.style.left = `${offset.left}px`;
+
+    setTarget(target);
+  }
+
+  function calculateTargetOffset(target) {
+    const { top, right, left, height } = target.getBoundingClientRect();
+
+    return {
+      top: isSmallScreen ? top + height * 2 : top - (height / 3) * 2,
+      left: isSmallScreen ? left : right + 30,
+    };
+  }
+
+  function screenHasBecomeSmall() {
+    return isCurrentWindowWidthSmall() && !isSmallScreen;
+  }
+
+  function screenHasBecomeBig() {
+    return isCurrentWindowWidthBig() && isSmallScreen;
+  }
+
+  return {
+    move,
+    target,
+    setTarget,
+    isSmallScreen,
+  };
+}
+
+export default usePopoverPosition;
